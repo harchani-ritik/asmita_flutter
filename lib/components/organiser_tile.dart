a9f56f7137d0aa7ce18bd09f4652dba078f8fa45
Ritik Harchani <harchani.ritik@gmail.com>
2020-02-06 23:36:00 +0530

    Data Updates

--- lib/components/organiser_tile.dart
+++ lib/components/organiser_tile.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class OrganiserTile extends StatelessWidget {
-
   final String imageUrl;
   final String name;
   final String designation;
   final String phoneNumber;
 
-  OrganiserTile({this.imageUrl,this.name,this.designation,this.phoneNumber});
+  OrganiserTile({this.imageUrl, this.name, this.designation, this.phoneNumber});
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +18,22 @@ class OrganiserTile extends StatelessWidget {
       ),
       title: Text(name),
       subtitle: Text(designation),
-      trailing: FlatButton(child:Icon(
-          Icons.call
-      ), onPressed: () {
-        launch("tel: $phoneNumber");
-      },),
+      trailing: FlatButton(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Icon(
+              Icons.call,
+            ),
+          ),
+        ),
+        onPressed: () {
+          launch("tel: $phoneNumber");
+        },
+      ),
     );
   }
 }
